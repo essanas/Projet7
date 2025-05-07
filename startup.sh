94161361e39ce38fa525ec13c8b1fae8c924d173
essanas <esseddiki.a@gmail.com>
2025-05-07 18:45:58 +0200

    Corriger startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@
 # Active l'environnement virtuel
 source venv/bin/activate
 
-# Lance l'application avec gunicorn et uvicorn
-gunicorn -k uvicorn.workers.UvicornWorker main:app --host 0.0.0.0 --port 8000
+# Lance l'application FastAPI avec Uvicorn
+uvicorn main:app --host 0.0.0.0 --port 80
