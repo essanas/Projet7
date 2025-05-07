0adc73cccf5a37400d4e5e470f4c6be4d40441d1
essanas <esseddiki.a@gmail.com>
2025-05-07 17:30:35 +0200

    Fix deployment configuration for Azure

--- startup.sh
+++ startup.sh
@@ -1,1 +1,7 @@
-uvicorn main:app --host 0.0.0.0 --port 8000
+#!/bin/bash
+
+# Active l'environnement virtuel
+source venv/bin/activate
+
+# Lance l'application avec gunicorn et uvicorn
+gunicorn -k uvicorn.workers.UvicornWorker main:app --host 0.0.0.0 --port 8000
